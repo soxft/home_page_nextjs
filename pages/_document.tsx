f6999892c890c9a969e31ce93ea4eed440fb3f13
xcsoft <email@xcsoft.top>
2022-04-14 15:36:01 +0800

    style: change favicon url

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="icon" href="https://cdn.timeletters.cn/img/xcsoft.jpg" />
+                    <link rel="icon" href="https://cdn.iirose.cn/xcsoft.jpg" />
                     <meta name="author" content="xcsoft" />
                     <meta name="description" content="xcsoft的个人主页, 原諒我這一生 不羁放纵爱自由" />
                     <meta name="keywords" content="xcsoft,星辰日记,soxft,xusoft" />
