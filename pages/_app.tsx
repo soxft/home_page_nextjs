707c4f15faec3414673afe9fd8e75ed9c59bad8c
soxft <email@xcsoft.top>
2022-06-10 15:40:25 +0800

    feta: edit analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -67,7 +67,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Script
         defer
         data-domain="xsot.cn"
-        src="https://stats.9420.ltd/stats/script.js"
+        src="https://s1.9420.ltd/script"
       />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
